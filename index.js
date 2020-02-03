c8d11a5d9305f233a5d9e3a61260245ccea7de9f
Even <even.juberg@ntnu.no>
2020-02-03 13:00:00 +0100

    Initial and probably final commit.

---
+++ index.js
@@ -0,0 +1,25 @@
+function calculateService () {
+  let service = document.getElementById('service')
+  let selected = service.options[service.selectedIndex].value
+  console.log(selected)
+  return selected
+}
+
+function getBill () {
+  let billing = document.getElementById('bill')
+  bill = billing.value
+  return bill
+}
+function sharing () {
+  let sharing = document.getElementById('sharing')
+  share = sharing.value
+  return share
+}
+
+let submit = document.getElementById('submit')
+submit.addEventListener('click', function () {
+  let service = calculateService()
+  let bill = getBill()
+  let share = sharing()
+  console.log('Your share is: ' + ((bill / share) * service))
+})
